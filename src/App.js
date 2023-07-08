4890ce47b4b8daa78a5f6d65578e92cfb012c1b7
Andrey <KharchenkoAndru@gmail.com>
2023-07-08 21:31:55 +0300

    Компонент Transition

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Transition } from 'react-transition-group';
 
 function App() {
   const [loaderVisible, setLoaderVisible] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setLoaderVisible(true), 1000);
+    setTimeout(() => setLoaderVisible(false), 5000);
+  }, [])
   return (
     <div className="app">
       <button onClick={() => setLoaderVisible(!loaderVisible)}>{loaderVisible ? "HIDE" : "SHOW"}</button>
       <div className="wrap">
-        { loaderVisible && <div className="circle" /> }
+        <Transition
+          in={loaderVisible}
+          timeout={500}
+          mountOnEnter
+          unmountOnExit
+          onEnter={() => console.log('onEnter')}
+          onEntering={() => console.log('onEntering')}
+          onEntered={() => console.log('onEntered')}
+          onExit={() => console.log('onExit')}
+          onExiting={() => console.log('onExiting')}
+          onExited={() => console.log('onExited')}
+        >
+          { state => <div className={`circle ${state}`} /> }
+        </Transition>
+      
       </div>
     </div>
   );
