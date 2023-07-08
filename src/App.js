2a9e2f191c170dd5d6cf358df931124514872f6c
Andrey <KharchenkoAndru@gmail.com>
2023-07-08 23:04:36 +0300

    Компонент TransitionGroup

--- src/App.js
+++ src/App.js
@@ -1,69 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
-import { Transition, CSSTransition, SwitchTransition } from 'react-transition-group';
+import { useState } from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
 
 function App() {
-  //const [loaderVisible, setLoaderVisible] = useState(false);
-  const [mode, setMode] = useState("out-in");
-  const [toggle, setToggle] = useState(false);
-
-  const changeHandler = (e) => {
-    setMode(e.target.value);
-  }
-  
+  const [text, setText] = useState("");
+  const [todoList, setTodoList] = useState([{ id: 1, text: "Пункт 1" }, { id: 2, text: "Пункт 2" }, { id: 3, text: "Пункт 3" }])
   return (
     <div className="app">
-      {/*<button onClick={() => setLoaderVisible(!loaderVisible)}>{loaderVisible ? "HIDE" : "SHOW"}</button>*/}
-      <div className="wrap">
-        {/*<Transition
-          in={loaderVisible}
-          timeout={500}
-          mountOnEnter
-          unmountOnExit
-          onEnter={() => console.log('onEnter')}
-          onEntering={() => console.log('onEntering')}
-          onEntered={() => console.log('onEntered')}
-          onExit={() => console.log('onExit')}
-          onExiting={() => console.log('onExiting')}
-          onExited={() => console.log('onExited')}
-        >
-          { state => <div className={`circle ${state}`} /> }
-        </Transition>*/}
-
-        {/*<CSSTransition
-          in={loaderVisible}
-          timeout={500}
-          classNames={{
-            enterActive: 'circle-show',
-            enterDone: 'circle-rotate',
-            exitActive: 'circle-hide',
-          }}
-          mountOnEnter
-          unmountOnExit
-        >
-          <div className="circle" />
-        </CSSTransition>*/}
+      <div>
+        <input onChange={(e) => setText(e.target.value)} value={text} type="text" />
+        <button onClick={() => setTodoList([...todoList, { id: todoList.length + 1, text: text }])}>Добавить</button>
       </div>
 
-      <div>
-          <label htmlFor='out-in'>out-in</label>
-          <input onChange={(e) => changeHandler(e)}  id="out-in" value="out-in" name='radio' type='radio' />
-          <label htmlFor='in-out'>in-out</label>
-          <input onChange={(e) => changeHandler(e)} id="in-out" value="in-out" name='radio' type='radio' />
-        </div>
-
-        <SwitchTransition mode={mode}>
-          <CSSTransition
-            timeout={500}
-            classNames="fade"
-            key={toggle}
-          >
-            <button onClick={() => setToggle(!toggle)}>
-            {toggle ? "Hello world" : "Goodbye world"}
-            </button>
-          </CSSTransition>
-        </SwitchTransition>
+      <TransitionGroup component='ul'>
+        {
+          todoList.map(({ id, text }) => (
+            <CSSTransition
+              key={id}
+              timeout={500}
+              classNames="todo"
+            >
+              <li className='todo' onClick={() => setTodoList([...todoList.filter(it => it.id != id)])}>{id} {text}</li>
+            </CSSTransition>
+          ))
+        }
+      </TransitionGroup>
     </div>
   );
 }
