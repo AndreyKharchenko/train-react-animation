c5904ca016b4b7985562dbc499dde65923dff8ab
Andrey <KharchenkoAndru@gmail.com>
2023-07-08 22:38:46 +0300

    Компонент SwitchTransition

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
-import { Transition } from 'react-transition-group';
+import { Transition, CSSTransition, SwitchTransition } from 'react-transition-group';
 
 function App() {
-  const [loaderVisible, setLoaderVisible] = useState(false);
+  //const [loaderVisible, setLoaderVisible] = useState(false);
+  const [mode, setMode] = useState("out-in");
+  const [toggle, setToggle] = useState(false);
 
-  useEffect(() => {
-    setTimeout(() => setLoaderVisible(true), 1000);
-    setTimeout(() => setLoaderVisible(false), 5000);
-  }, [])
+  const changeHandler = (e) => {
+    setMode(e.target.value);
+  }
+  
   return (
     <div className="app">
-      <button onClick={() => setLoaderVisible(!loaderVisible)}>{loaderVisible ? "HIDE" : "SHOW"}</button>
+      {/*<button onClick={() => setLoaderVisible(!loaderVisible)}>{loaderVisible ? "HIDE" : "SHOW"}</button>*/}
       <div className="wrap">
-        <Transition
+        {/*<Transition
           in={loaderVisible}
           timeout={500}
           mountOnEnter
@@ -27,9 +29,41 @@ function App() {
           onExited={() => console.log('onExited')}
         >
           { state => <div className={`circle ${state}`} /> }
-        </Transition>
-      
+        </Transition>*/}
+
+        {/*<CSSTransition
+          in={loaderVisible}
+          timeout={500}
+          classNames={{
+            enterActive: 'circle-show',
+            enterDone: 'circle-rotate',
+            exitActive: 'circle-hide',
+          }}
+          mountOnEnter
+          unmountOnExit
+        >
+          <div className="circle" />
+        </CSSTransition>*/}
       </div>
+
+      <div>
+          <label htmlFor='out-in'>out-in</label>
+          <input onChange={(e) => changeHandler(e)}  id="out-in" value="out-in" name='radio' type='radio' />
+          <label htmlFor='in-out'>in-out</label>
+          <input onChange={(e) => changeHandler(e)} id="in-out" value="in-out" name='radio' type='radio' />
+        </div>
+
+        <SwitchTransition mode={mode}>
+          <CSSTransition
+            timeout={500}
+            classNames="fade"
+            key={toggle}
+          >
+            <button onClick={() => setToggle(!toggle)}>
+            {toggle ? "Hello world" : "Goodbye world"}
+            </button>
+          </CSSTransition>
+        </SwitchTransition>
     </div>
   );
 }
